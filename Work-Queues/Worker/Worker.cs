4827df2a358fd4d5f3225a60c175e501f18491a7
Jonathan Vieira <jonathansouza59@gmail.br>
2022-05-25 20:35:37 -0300

    feat: Work and queues.

---
+++ Work-Queues/Worker/Worker.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+class Receive  {
+    public static void Main() {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare(queue: "task_queue",
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+            
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            consumer.Received += (model, ea) => {
+                
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Receive {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "task_queue",
+                                 autoAck: false,
+                                 consumer: consumer);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
